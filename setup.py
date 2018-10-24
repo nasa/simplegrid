17a1853d9d4ccf32b5d52c7ab25bf9b63af71398
Greg Moore <Gregory.J.Moore@jpl.nasa.gov>
2018-10-24 11:17:32 -0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,29 @@
+
+from setuptools import setup, find_packages
+from os import path
+from io import open
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name        = 'simplegrid',
+    version     = '1.0',
+    description = 'Simple local grid creation and refinement for ocean circulation models',
+    long_description                = long_description,
+    long_description_content_type   = 'text/x-rst',
+    packages    = find_packages(exclude=['*tests*']),
+    install_requires = [
+        'numpy',
+        'pyproj',
+    ],
+    entry_points = {
+        'console_scripts': [
+            'simplegrid=simplegrid.mkgrid:main',
+            'simpleregrid=simplegrid.regrid:main',
+        ],
+    },
+)
