db1e0ba1fa7c9d20747121664d8421adaf31b6f8
Greg Moore <greg.moore@jpl.nasa.gov>
2018-11-14 14:11:58 -0800

    Added --xg_file, --yg_file csv and binary input options, reworked command-line structure to accommodate, added corresponding test cases.

--- simplegrid/tests/test_regrid.py
+++ simplegrid/tests/test_regrid.py
@@ -11,20 +11,20 @@ class TestRegrid(unittest.TestCase):
         simpleregrid command-line call:
 
         simpleregrid \
-            ./data/tile005.mitgrid \
-            270 \
-            90 \
-            -126.95921008 \
-            67.31607921 \
-            -126.21154908 \
-            67.17736362 \
-            1 \ 
-            1 \ 
-            regrid_test_1.mitgrid
+            --mitgridfile ./data/tile005.mitgrid \
+            --ni 270 \
+            --nj  90 \
+            --lon1 -126.95921008 \
+            --lat1   67.31607921 \
+            --lon2 -126.21154908 \
+            --lat2   67.17736362 \
+            --lon_subscale 1 \
+            --lat_subscale 1 \
+            --outfile regrid_test_1.mitgrid
         """
 
         (newgrid,newgrid_ni,newgrid_nj) = sg.regrid.regrid(
-            './data/tile005.mitgrid', 270, 90, 
+            './data/tile005.mitgrid', None, None, 270, 90, 
             -126.959, 67.316, -126.212, 67.177,
             1, 1)
 
@@ -41,20 +41,20 @@ class TestRegrid(unittest.TestCase):
         simpleregrid command-line call:
 
         simpleregrid \
-            ./data/tile005.mitgrid \
-            270 \
-            90 \
-            -126.95921008 \
-            67.31607921 \
-            -126.21154908 \
-            67.17736362 \
-            30 \ 
-            20 \ 
-            regrid_test_2.mitgrid
+            --mitgridfile ./data/tile005.mitgrid \
+            --ni 270 \
+            --nj  90 \
+            --lon1 -126.95921008 \
+            --lat1   67.31607921 \
+            --lon2 -126.21154908 \
+            --lat2   67.17736362 \
+            --lon_subscale 30 \
+            --lat_subscale 20 \
+            --outfile regrid_test_2.mitgrid
         """
 
         (newgrid,newgrid_ni,newgrid_nj) = sg.regrid.regrid(
-            './data/tile005.mitgrid', 270, 90, 
+            './data/tile005.mitgrid', None, None, 270, 90, 
             -126.959, 67.316, -126.212, 67.177,
             30, 20)
 
@@ -64,26 +64,27 @@ class TestRegrid(unittest.TestCase):
         for a,b in zip(newgrid,validated_grid):
             nptest.assert_array_equal(newgrid[a],validated_grid[b])
 
+
     def test_regrid_3(self):
         """ Tests 1x1 'remeshing' of a 1x1 corner cell from an llc 90 model
         tile.  Results are compared against validated mitgrid file created using
         simpleregrid command-line call:
 
         simpleregrid \
-            ./data/tile005.mitgrid \
-            270 \
-            90 \
-            -128.0 \
-            67.5 \
-            -127.58893247 \
-            67.42868828 \
-            1 \ 
-            1 \ 
-            regrid_test_3.mitgrid
+            --mitgridfile ./data/tile005.mitgrid \
+            --ni 270 \
+            --nj  90 \
+            --lon1 -128.0 \
+            --lat1   67.5 \
+            --lon2 -127.58893247 \
+            --lat2   67.42868828 \
+            --lon_subscale 1 \
+            --lat_subscale 1 \
+            --outfile regrid_test_3.mitgrid
         """
 
         (newgrid,newgrid_ni,newgrid_nj) = sg.regrid.regrid(
-            './data/tile005.mitgrid', 270, 90, 
+            './data/tile005.mitgrid', None, None, 270, 90, 
             -128., 67.5, -127.58893247, 67.42868828,
             1, 1)
 
@@ -92,6 +93,71 @@ class TestRegrid(unittest.TestCase):
         # individual comparison of dictionary-stored numpy arrays:
         for a,b in zip(newgrid,validated_grid):
             nptest.assert_array_equal(newgrid[a],validated_grid[b])
+
+
+    def test_regrid_4(self):
+        """ Tests 1x1 'remeshing' of a 1x1 cell from an llc 90 model tile.
+        Similar to test_regrid_1, but uses --xg_file, --yg_file option with
+        binary input.  Results are compared against validated mitgrid file
+        created using simpleregrid command-line call:
+
+        simpleregrid \
+            --xg_file tile005_XG.bin \
+            --yg_file tile005_YG.bin \
+            --ni 270 \
+            --nj  90 \
+            --lon1 -126.95921008 \
+            --lat1   67.31607921 \
+            --lon2 -126.21154908 \
+            --lat2   67.17736362 \
+            --lon_subscale 1 \
+            --lat_subscale 1 \
+            --outfile regrid_test_4.mitgrid
+        """
+
+        (newgrid,newgrid_ni,newgrid_nj) = sg.regrid.regrid(
+            None, './data/tile005_XG.bin', './data/tile005_YG.bin', 270, 90, 
+            -126.959, 67.316, -126.212, 67.177,
+            1, 1)
+
+        validated_grid = sg.gridio.read_mitgridfile('./data/regrid_test_4.mitgrid',1,1)
+
+        # individual comparison of dictionary-stored numpy arrays:
+        for a,b in zip(newgrid,validated_grid):
+            nptest.assert_array_equal(newgrid[a],validated_grid[b])
+
+
+    def test_regrid_5(self):
+        """ Tests 1x1 'remeshing' of a 1x1 cell from an llc 90 model tile.
+        Similar to test_regrid_4, but uses --xg_file, --yg_file option with
+        *.csv input.  Results are compared against validated mitgrid file
+        created using simpleregrid command-line call:
+
+        simpleregrid \
+            --xg_file tile005_XG.csv \
+            --yg_file tile005_YG.csv \
+            --ni 270 \
+            --nj  90 \
+            --lon1 -126.95921008 \
+            --lat1   67.31607921 \
+            --lon2 -126.21154908 \
+            --lat2   67.17736362 \
+            --lon_subscale 1 \
+            --lat_subscale 1 \
+            --outfile regrid_test_4.mitgrid
+        """
+
+        (newgrid,newgrid_ni,newgrid_nj) = sg.regrid.regrid(
+            None, './data/tile005_XG.csv', './data/tile005_YG.csv', 270, 90, 
+            -126.959, 67.316, -126.212, 67.177,
+            1, 1)
+
+        validated_grid = sg.gridio.read_mitgridfile('./data/regrid_test_5.mitgrid',1,1)
+
+        # individual comparison of dictionary-stored numpy arrays:
+        for a,b in zip(newgrid,validated_grid):
+            nptest.assert_array_equal(newgrid[a],validated_grid[b])
+
 
 if __name__=='__main__':
     unittest.main()
