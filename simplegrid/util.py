1aff5bdf231f9e0ac603be326cee405f0e8c6514
Greg Moore <greg.moore@jpl.nasa.gov>
2018-12-05 15:01:02 -0800

    New edge matching utility to support 'add_fringe' and 'stitch' functionality.

--- simplegrid/util.py
+++ simplegrid/util.py
@@ -30,6 +30,117 @@ def lonlat2cart( lons, lats, rad=1.):
     return cart
 
 
+def matchedges( aXG, aYG, bXG, bYG, geod, verbose=False):
+    """Given two tiles, find their matching edges.
+
+    Args:
+        aXG, aYG (numpy arrays): Grid point longitudes and latitudes,
+            respectively, for the first tile (mitgridfile format).
+        bXG, bYG (numpy arrays): Grid point longitudes and latitudes,
+            respectively, for the second tile (mitgridfile format).
+        geod (pyproj.Geod object): Geod to be used as basis for distance
+            calculations.
+        verbose (logical): verbose output
+
+    Returns:
+        (a_edge,b_edge) tuple of slice object pairs that will yield the input
+            tile matching edge grids, e.g, for an east-west match between 'a'
+            and 'b', ((numpy.s_[-1],numpy.s_[:]),(numpy.s_[0],numpy.s_[:]))
+
+    Raises:
+        ValueErrors if matching edges are not found, are not sufficiently close,
+        or are inconsistent (e.g., incorrect alignment)
+
+    Note:
+        Input tiles are assumed to have the same orientation.
+        
+    """
+
+    # some useful parameters:
+    edges   = (N,S,E,W)     = (1,0,1,0)                     # j_n,j_s,i_e,i_w
+    corners = (SW,SE,NE,NW) = (0,0),(-1,0),(-1,-1),(0,-1)   # full matrix idxs
+    CLOSE_ENOUGH = 1.e-6                                    # error checking
+    a_rtn  = b_rtn = None                                   # return vals
+
+    # determine nominal diagonal distance for error checking:
+    _,_,a_swne_dist = geod.inv(aXG[SW],aYG[SW],aXG[NE],aYG[NE])
+    _,_,a_senw_dist = geod.inv(aXG[SE],aYG[SE],aXG[NW],aYG[NW])
+    _,_,b_swne_dist = geod.inv(bXG[SW],bYG[SW],bXG[NE],bYG[NE])
+    _,_,b_senw_dist = geod.inv(bXG[SE],bYG[SE],bXG[NW],bYG[NW])
+    nom_diag = (a_swne_dist+a_senw_dist+b_swne_dist+b_senw_dist)/4.
+
+    # build 2x2 corner matrices of minimum distances between a and b:
+    a_to_b_dist = np.full((2,2),np.inf)
+    b_to_a_dist = a_to_b_dist.copy()
+
+    for corner_a in corners:
+        for corner_b in corners:
+            _,_,dist = geod.inv(
+                aXG[corner_a], aYG[corner_a], bXG[corner_b], bYG[corner_b])
+            if dist < a_to_b_dist[corner_a]:
+                # keep track of minimum distances without regard to the actual
+                # corner in b that's closest...:
+                a_to_b_dist[corner_a] = dist
+                # ...and, by reciprocity:
+                b_to_a_dist[corner_b] = a_to_b_dist[corner_a]
+
+    # make sure minimums are sufficently close to zero:
+    if np.amin([np.amin(a_to_b_dist),np.amin(b_to_a_dist)]) > nom_diag*CLOSE_ENOUGH:
+        raise ValueError('Input tiles not sufficiently close to determine common edge')
+
+    # edge detection:
+
+    a_to_b_rowmins = np.argmin(a_to_b_dist,axis=0).tolist()
+    a_to_b_colmins = np.argmin(a_to_b_dist,axis=1).tolist()
+    if a_to_b_rowmins == [W,W]:
+        a_edge, a_rtn = W, (np.s_[0],np.s_[:])
+        if verbose:
+            msg = "western edge of tile 'a' "
+    elif a_to_b_rowmins == [E,E]:
+        a_edge, a_rtn = E, (np.s_[-1],np.s_[:])
+        if verbose:
+            msg = "eastern edge of tile 'a' "
+    elif a_to_b_colmins == [S,S]:
+        a_edge, a_rtn = S, (np.s_[:],np.s_[0])
+        if verbose:
+            msg = "southern edge of tile 'a' "
+    elif a_to_b_colmins == [N,N]:
+        a_edge, a_rtn = N, (np.s_[:],np.s_[-1])
+        if verbose:
+            msg = "northern edge of tile 'a' "
+    else:
+        raise ValueError('No matching edge found for first input tile')
+
+    b_to_a_rowmins = np.argmin(b_to_a_dist,axis=0).tolist()
+    b_to_a_colmins = np.argmin(b_to_a_dist,axis=1).tolist()
+    if b_to_a_rowmins == [W,W]:
+        b_edge, b_rtn = W, (np.s_[0],np.s_[:])
+        if verbose:
+            msg = msg + "matches western edge of tile 'b'"
+    elif b_to_a_rowmins == [E,E]:
+        b_edge, b_rtn = E, (np.s_[-1],np.s_[:])
+        if verbose:
+            msg = msg + "matches eastern edge of tile 'b'"
+    elif b_to_a_colmins == [S,S]:
+        b_edge, b_rtn = S, (np.s_[:],np.s_[0])
+        if verbose:
+            msg = msg + "matches southern edge of tile 'b'"
+    elif b_to_a_colmins == [N,N]:
+        b_edge, b_rtn = N, (np.s_[:],np.s_[-1])
+        if verbose:
+            msg = msg + "matches northern edge of tile 'b'"
+    else:
+        raise ValueError('No matching edge found for second input tile')
+
+    if a_edge+b_edge != 1:
+        raise ValueError('Inconsistent edge match for input grids')
+
+    if verbose:
+        print(msg)
+
+    return (a_rtn,b_rtn)
+
+
 def nearest(lon,lat,lons,lats,geod):
     """Determine indices of, and distance to, nearest lon/lat point.
 
