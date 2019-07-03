d15da9846c2d8a414c9f165012fb189b53b6f893
Greg Moore <greg.moore@jpl.nasa.gov>
2019-07-02 17:05:16 -0700

    Cooler version of run_make_grid_files.sh.

--- simplegrid/tests/data/run_make_grid_files.sh
+++ simplegrid/tests/data/run_make_grid_files.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-# create test grid files in dependent order:
+# create test grid files in prescribed order
 
-make_mkgrid_files.sh && \
-make_regrid_files.sh && \
-make_fringe_and_stitch_files.sh && \
-make_stitch_files.sh && \
-make_addfringe_files.sh
+mkgrids=(\
+    make_mkgrid_files.sh \
+    make_regrid_files.sh \
+    make_fringe_and_stitch_files.sh \
+    make_stitch_files.sh \
+    make_addfringe_files.sh)
+
+for i in ${mkgrids[@]}; do
+    echo "$i..." && $i
+done
+
